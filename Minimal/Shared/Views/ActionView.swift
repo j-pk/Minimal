7e64cceebacc281861a593e3828cd242b0a76b12
Jameson Parker Kirby <jpk@jamesonsmacbook>
2018-05-20 18:31:58 -0400

    - added animation and haptic feedback to actionView

--- Minimal/Shared/Views/ActionView.swift
+++ Minimal/Shared/Views/ActionView.swift
@@ -17,8 +17,10 @@ class ActionView: XibView {
     @IBOutlet weak var moreButton: UIButton!
     
     @IBAction func didSelectUpvoteButton(_ sender: UIButton) {
+        applyShakeAnimationAndHapticFeedback(toButton: sender)
     }
     @IBAction func didSelectDownvoteButton(_ sender: UIButton) {
+        applyShakeAnimationAndHapticFeedback(toButton: sender)
     }
     @IBAction func didSelectPageDownButton(_ sender: UIButton) {
     }
@@ -27,4 +29,27 @@ class ActionView: XibView {
     @IBAction func didSelectMoreButton(_ sender: UIButton) {
     }
     
+    func applyShakeAnimationAndHapticFeedback(toButton button: UIButton) {
+        let notification = UINotificationFeedbackGenerator()
+        notification.notificationOccurred(.success)
+        
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.10
+        shake.repeatCount = 1
+        shake.autoreverses = true
+        
+        let distance: CGFloat = 8
+        let direction = button == upvoteButton ? (button.center.y - distance) : (button.center.y + distance)
+        
+        let fromPoint = CGPoint(x: button.center.x, y: button.center.y)
+        let fromValue = NSValue(cgPoint: fromPoint)
+        
+        let toPoint = CGPoint(x: button.center.x, y: direction)
+        let toValue = NSValue(cgPoint: toPoint)
+        
+        shake.fromValue = fromValue
+        shake.toValue = toValue
+        
+        button.layer.add(shake, forKey: "position")
+    }
 }
