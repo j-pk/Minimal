174d9e18c7a334fec3bb34b8e70c2c19af8ea429
Jameson Parker Kirby <jpk@jamesons-macbook.local>
2018-10-14 21:54:56 -0400

    - added some enum property values to notification state - clean up

--- Minimal/Shared/Custom/NotificationBanner.swift
+++ Minimal/Shared/Custom/NotificationBanner.swift
@@ -26,6 +26,32 @@ enum NotificationState {
         case .message: return .gray
         }
     }
+    
+    var message: String {
+        switch self {
+        case .vote(let direction):
+            switch direction {
+            case .up: return "Upvote successful"
+            case .down: return "Downvote successful"
+            case .cancel: return "Canceled vote"
+            }
+        case .error(let error): return error
+        case .message(let message): return message
+        }
+    }
+    
+    var image: UIImage {
+        switch self {
+        case .vote(let direction):
+            switch direction {
+            case .up: return #imageLiteral(resourceName: "upArrow")
+            case .down: return #imageLiteral(resourceName: "downArrow")
+            case .cancel: return #imageLiteral(resourceName: "checkmark")
+            }
+        case .error: return #imageLiteral(resourceName: "minimalSmall")
+        case .message: return #imageLiteral(resourceName: "minimalSmall")
+        }
+    }
 }
 
 class NotificationView: UIView {
@@ -34,6 +60,15 @@ class NotificationView: UIView {
     var initialPositionConstraint: NSLayoutConstraint?
     let height: CGFloat = 45
 
+    override var tag: Int {
+        get {
+            return 777
+        }
+        set {
+            
+        }
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -51,17 +86,21 @@ class NotificationView: UIView {
         layer.masksToBounds = true
     }
     
-    func configureNotificationView() {
+    private func configureNotificationView() {
         guard let viewController = viewController else { return }
+        guard !viewController.view.subviews.contains(where: { $0.tag == tag }) else { return }
         addRemoveNotificationGesture()
         backgroundColor = state.backgroundColor
         
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "checkmark"))
+        let imageView = UIImageView(image: state.image)
+        imageView.tintColor = .black
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
         let descriptionLabel = UILabel()
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.text = "Banner"
+        descriptionLabel.text = state.message
+        descriptionLabel.backgroundColor = .clear
+        
         [imageView, descriptionLabel].forEach({ addSubview($0) })
         
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[imageView(20)]",
@@ -83,10 +122,12 @@ class NotificationView: UIView {
         initialPositionConstraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: viewController.view, attribute: .top, multiplier: 1, constant: -100)
         guard let initialPositionConstraint = initialPositionConstraint else { return }
         initialPositionConstraint.priority = UILayoutPriority(rawValue: 999)
+        viewController.view.addConstraint(initialPositionConstraint)
         
-        let displayPositionConstraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: viewController.view, attribute: .top, multiplier: 1, constant: viewController.view.safeAreaInsets.top)
-        displayPositionConstraint.priority = UILayoutPriority(rawValue: 998)
-        viewController.view.addConstraints([initialPositionConstraint, displayPositionConstraint])
+        viewController.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-\(viewController.view.safeAreaInsets.top)@998-[view(\(height))]",
+                                                                                   options: NSLayoutConstraint.FormatOptions.alignAllCenterX,
+                                                                                   metrics: nil,
+                                                                                     views: ["view": self]))
 
         viewController.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-\(viewController.view.layoutMargins.left)-[view]-\(viewController.view.layoutMargins.right)-|",
                                                                                    options: NSLayoutConstraint.FormatOptions.alignAllCenterY,
@@ -95,7 +136,7 @@ class NotificationView: UIView {
         animate(constraint: initialPositionConstraint, in: viewController)
     }
     
-    func animate(constraint: NSLayoutConstraint, in viewController: UIViewController) {
+    private func animate(constraint: NSLayoutConstraint, in viewController: UIViewController) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2, options: [.curveEaseIn], animations: {
                 constraint.priority = UILayoutPriority(rawValue: 997)
@@ -106,7 +147,7 @@ class NotificationView: UIView {
         animateRemoval(constraint: constraint, in: viewController, forDuration: 3.0)
     }
     
-    func animateRemoval(constraint: NSLayoutConstraint, in viewController: UIViewController, forDuration duration: Double) {
+    private func animateRemoval(constraint: NSLayoutConstraint, in viewController: UIViewController, forDuration duration: Double) {
         DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.2, options: [.curveEaseOut], animations: {
                 constraint.priority = UILayoutPriority(rawValue: 999)
@@ -117,13 +158,13 @@ class NotificationView: UIView {
         }
     }
     
-    func addRemoveNotificationGesture() {
+    private func addRemoveNotificationGesture() {
         let tapAwayGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(removeNotification(sender:)))
         isUserInteractionEnabled = true
         addGestureRecognizer(tapAwayGestureRecognizer)
     }
     
-    @objc func removeNotification(sender: UITapGestureRecognizer) {
+    @objc private func removeNotification(sender: UITapGestureRecognizer) {
         guard let constraint = initialPositionConstraint, let viewController = viewController else { return }
         animateRemoval(constraint: constraint, in: viewController, forDuration: 0.0)
     }
